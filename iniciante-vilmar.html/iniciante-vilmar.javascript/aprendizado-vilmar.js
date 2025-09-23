91273f42dbc9801b907c179d5eb01fb13e35eccc
Vilmar Freire <paipedroservino@gmail.com>
2025-09-23 01:09:14 -0300

    Fiz-mais-uma-alteração.

--- iniciante-vilmar.html/iniciante-vilmar.javascript/aprendizado-vilmar.js
+++ iniciante-vilmar.html/iniciante-vilmar.javascript/aprendizado-vilmar.js
@@ -1,5 +1,11 @@
 document.writeln("<h1>Olá Mundo!</h1>")
+var array_1 = new Array()
 var i
 for(i = 0; i < 10; i++){
-    document.writeln('contando:' + i + '<br>')
+    array_1[i] = 0
+
+}
+
+for(i = 0; i < 10; i++){
+    document.writeln('contando: ' + array_1[i] + '<br>')
 }
